bce68806e46af2df4d5e55b07fff1fffe5fe936a
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-02-14 21:26:40 +0100

    Feature: API key

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,25 @@
 <script>
 export default {
   name: 'app'
+  data(){
+    return{
+      api_key: '582f58018e28370809832a36ffc87799'
+    }
 }
 </script>
 
 <style lang='scss' scoped>
 
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 16px 0px 16px 0px;
+
+}
 </style>
